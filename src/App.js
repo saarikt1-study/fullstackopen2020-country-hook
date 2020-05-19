3676a28f3df272c58572a600c7ee0fbe63b71c97
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-05-19 09:37:38 +0200

    Refactor 7.7

--- src/App.js
+++ src/App.js
@@ -22,7 +22,10 @@ const useCountry = (name) => {
     axios
       .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
       .then(response => {
-        setCountry(response)
+        setCountry({
+          found: true,
+          data: response.data[0]
+        })
       })
   }, [name])
 
@@ -34,20 +37,20 @@ const Country = ({ country }) => {
     return null
   }
 
-  // if (!country.found) {
-  //   return (
-  //     <div>
-  //       not found...
-  //     </div>
-  //   )
-  // }
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h3>{country.data[0].name} </h3>
-      <div>capital {country.data[0].capital} </div>
-      <div>population {country.data[0].population}</div> 
-      <img src={country.data[0].flag} height='100' alt={`flag of ${country.data[0].name}`}/>  
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
     </div>
   )
 }
